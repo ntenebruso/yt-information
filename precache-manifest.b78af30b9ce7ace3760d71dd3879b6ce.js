846810e4d24d1c0a31d5b6521ed21cdd4ab84d5b
nickforthewin <37882775+nickforthewin@users.noreply.github.com>
2020-04-10 12:58:51 -0400

    Updates

---
+++ precache-manifest.b78af30b9ce7ace3760d71dd3879b6ce.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b82e53b0e38ed7f3a98da5444b1bbec7",
+    "url": "/yt-information/index.html"
+  },
+  {
+    "revision": "c5e15a5458507900225c",
+    "url": "/yt-information/static/css/main.9a8b2e07.chunk.css"
+  },
+  {
+    "revision": "3cda2568e82e59886e09",
+    "url": "/yt-information/static/js/2.5c4ee218.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/yt-information/static/js/2.5c4ee218.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c5e15a5458507900225c",
+    "url": "/yt-information/static/js/main.43f2ec60.chunk.js"
+  },
+  {
+    "revision": "351112a67b02f1619279",
+    "url": "/yt-information/static/js/runtime-main.b454c7ce.js"
+  }
+]);
